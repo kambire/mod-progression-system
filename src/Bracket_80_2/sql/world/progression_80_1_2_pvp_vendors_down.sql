42eaf37b26c143a1709f6ff1690642d274abf0c4
Cesar Augusto Benitez <kambire2007@gmail.com>
2025-12-25 21:01:13 -0300

    Restore phasemask on PvP vendor unlock

--- src/Bracket_80_2/sql/world/progression_80_1_2_pvp_vendors_down.sql
+++ src/Bracket_80_2/sql/world/progression_80_1_2_pvp_vendors_down.sql
@@ -2,3 +2,6 @@ DELETE FROM `game_event_creature` WHERE `guid` IN (152028, 152031) AND `eventEnt
 INSERT INTO `game_event_creature` (`eventEntry`, `guid`) VALUES
 (57,152028),
 (57,152031);
+
+-- Restore vendors visibility for players (unhide from GM-only phasemask).
+UPDATE `creature` SET `phaseMask` = 1 WHERE `guid` IN (133920, 125693, 125691, 133917);
